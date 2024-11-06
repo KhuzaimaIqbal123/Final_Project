9e90ac30f33305f8b01017645c42289ac1a7116e
Khuzaima <114830749+KhuzaimaIqbal123@users.noreply.github.com>
2024-11-06 18:12:46 +0500

    Listing Card component rendered with its css

--- Airbnb/Airbnb/components/Categories.jsx
+++ Airbnb/Airbnb/components/Categories.jsx
@@ -1,14 +1,24 @@
 // Categories.js
-import React from 'react';
+import React, { useState } from 'react';
 import './Categories.css';
 
 function Categories() {
     const categories = ['Beachfront', 'Cabins', 'Trending', 'Mountains', 'City', 'Countryside', 'Lakeside', 'Luxury', 'Budget'];
+    const [activeCategory, setActiveCategory] = useState('');
+
+    const handleCategoryClick = (category) => {
+        setActiveCategory(category);
+        console.log("Selected category:", category); // You can add functionality here for filtering based on category
+    };
 
     return (
         <div className="categories">
             {categories.map((category, index) => (
-                <button key={index} className="category-button">
+                <button
+                    key={index}
+                    className={`category-button ${activeCategory === category ? 'active' : ''}`}
+                    onClick={() => handleCategoryClick(category)}
+                >
                     {category}
                 </button>
             ))}
--- Airbnb/Airbnb/src/App.jsx
+++ Airbnb/Airbnb/src/App.jsx
@@ -1,15 +1,29 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
-import SearchBar from '../components/SearchBar';
-import Categories from '../components/Categories';
+import React from "react";
+import Navbar from "../components/Navbar";
+import SearchBar from "../components/SearchBar";
+import Categories from "../components/Categories";
+import ListingCard from "../components/ListingCards";
 
 function App() {
+    const property = {
+        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTOA59hxXnn5bZ5TqDYCgEhv0gZ-Fv-uShzlA&s',
+        title: 'Beautiful Beach House',
+        type: 'Entire home',
+        guests: 4,
+        bedrooms: 2,
+        bathrooms: 1,
+        price: 150,
+        rating: 4.8
+    };
+
     return (
         <div>
             <Navbar />
-            <SearchBar/>
-            <Categories/>
-            {/* Other components or content */}
+            <SearchBar />
+            <Categories />
+            <div className="App">
+                <ListingCard property={property} />
+            </div>            
         </div>
     );
 }
